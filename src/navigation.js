d3bb5b7e1c15c10d248113d97107348c9bdefd29
Abhilash John <abhilashjohn07@gmail.com>
2025-12-06 19:56:50 +0530

    fonoa comparison

--- src/navigation.js
+++ src/navigation.js
@@ -2,116 +2,116 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-  /*  {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    }, 
-
-    */
+    /*  {
+        text: 'Homes',
+        links: [
+          {
+            text: 'SaaS',
+            href: getPermalink('/homes/saas'),
+          },
+          {
+            text: 'Startup',
+            href: getPermalink('/homes/startup'),
+          },
+          {
+            text: 'Mobile App',
+            href: getPermalink('/homes/mobile-app'),
+          },
+          {
+            text: 'Personal',
+            href: getPermalink('/homes/personal'),
+          },
+        ],
+      },
+      {
+        text: 'Pages',
+        links: [
+          {
+            text: 'Features (Anchor Link)',
+            href: getPermalink('/#features'),
+          },
+          {
+            text: 'Services',
+            href: getPermalink('/services'),
+          },
+          {
+            text: 'Pricing',
+            href: getPermalink('/pricing'),
+          },
+          {
+            text: 'About us',
+            href: getPermalink('/about'),
+          },
+          {
+            text: 'Contact',
+            href: getPermalink('/contact'),
+          },
+          {
+            text: 'Terms',
+            href: getPermalink('/terms'),
+          },
+          {
+            text: 'Privacy policy',
+            href: getPermalink('/privacy'),
+          },
+        ],
+      },
+      {
+        text: 'Landing',
+        links: [
+          {
+            text: 'Lead Generation',
+            href: getPermalink('/landing/lead-generation'),
+          },
+          {
+            text: 'Long-form Sales',
+            href: getPermalink('/landing/sales'),
+          },
+          {
+            text: 'Click-Through',
+            href: getPermalink('/landing/click-through'),
+          },
+          {
+            text: 'Product Details (or Services)',
+            href: getPermalink('/landing/product'),
+          },
+          {
+            text: 'Coming Soon or Pre-Launch',
+            href: getPermalink('/landing/pre-launch'),
+          },
+          {
+            text: 'Subscription',
+            href: getPermalink('/landing/subscription'),
+          },
+        ],
+      },
+      {
+        text: 'Blog',
+        links: [
+          {
+            text: 'Blog List',
+            href: getBlogPermalink(),
+          },
+          {
+            text: 'Article',
+            href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+          },
+          {
+            text: 'Article (with MDX)',
+            href: getPermalink('markdown-elements-demo-post', 'post'),
+          },
+          {
+            text: 'Category Page',
+            href: getPermalink('tutorials', 'category'),
+          },
+          {
+            text: 'Tag Page',
+            href: getPermalink('astro', 'tag'),
+          },
+        ],
+      }, 
+  
+      */
     {
       text: 'Features',
       href: '/#features',
@@ -128,27 +128,27 @@ export const headerData = {
       text: 'Resources',
       links: [
         {
-      text: 'Supported Countries',
-      href: '/countries',
-    },
+          text: 'Supported Countries',
+          href: '/countries',
+        },
         {
           text: 'Guides',
           href: getPermalink('https://lookuptax.com/docs/category/verify-tax-ids'),
         },
-       
-      /*  {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-        */
+
+        /*  {
+            text: 'Startup',
+            href: getPermalink('/homes/startup'),
+          },
+          {
+            text: 'Mobile App',
+            href: getPermalink('/homes/mobile-app'),
+          },
+          {
+            text: 'Personal',
+            href: getPermalink('/homes/personal'),
+          },
+          */
       ],
     },
     {
@@ -159,20 +159,20 @@ export const headerData = {
           href: '/integrations/shopify-taxid-validation',
         },
         {
-      text: 'n8n',
-      href: '/integrations/n8n-taxid-validation',
+          text: 'n8n',
+          href: '/integrations/n8n-taxid-validation',
+        },
+        {
+          text: 'Google Sheets',
+          href: '/integrations/google-sheets-tax-id-validation',
+        },
+        {
+          text: 'Microsoft Excel',
+          href: '/integrations/microsoft-excel-tax-id-validation',
+        }
+      ],
     },
     {
-      text: 'Google Sheets',
-      href: '/integrations/google-sheets-tax-id-validation',
-    },
-    {
-      text: 'Microsoft Excel',
-      href: '/integrations/microsoft-excel-tax-id-validation',
-    }
-],
-    },
-     {
       text: 'API\u00A0Docs',
       href: 'https://demo.lookuptax.com/api/',
     },
@@ -218,6 +218,7 @@ export const footerData = {
       links: [
         { text: 'About', href: '/about' },
         { text: 'Blog', href: '/blog' },
+        { text: 'LookupTax vs Fonoa', href: '/lookuptax-vs-fonoa' },
         /*{ text: 'Careers', href: '#' },
         { text: 'Press', href: '#' },
         { text: 'Inclusion', href: '#' },
@@ -233,10 +234,10 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/lookuptax' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/lookuptax/' },
-   /* { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },*/
+    /* { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },*/
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-   /* { ariaLabel: 'Github', icon: 'tabler:brand-github', href: '#' },*/
+    /* { ariaLabel: 'Github', icon: 'tabler:brand-github', href: '#' },*/
   ],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://lookuptax.com/"> Lookuptax</a> · All rights reserved.
